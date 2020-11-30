be249f0c793fa96f693bb5a71bf5290445a66308
JadJabb <74005562+JadJabb@users.noreply.github.com>
2020-11-30 10:38:24 +0200

    Delete categories.component.js

--- module4-solution/lib/categories.component.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    angular.module('MenuApp')
-             .component('categoryList', {      
-            templateUrl: 'src/Category/menu.template.html',
-            bindings: {
-                items: '<',
-            }
-        });
-})();
